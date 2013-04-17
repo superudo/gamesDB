a76e52585423cadfec4149968f6116007f4412e9
Udo Woitek <superudo@gmail.com>
2013-04-17 22:46:26 +0200

    PHP Code in Game-Formular, Autocomplete Publisher

--- protected/views/game/_form.php
+++ protected/views/game/_form.php
@@ -22,158 +22,79 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'name'); ?>
-		<?php echo $form->textField($model, 'name', array('maxlength' => 100)); ?>
-		<?php echo $form->error($model, 'name'); ?>
-	</div>
-
-	<div class="row">		<?php 
+		<?php 
+			echo $form->labelEx($model, 'name');
+			echo $form->textField($model, 'name', array('maxlength' => 100));
 			echo CHtml::button("luding lookup", array( 
 				'title' => 'luding_lookup', 
 				'onclick' => "window.open('http://sunsite.informatik.rwth-aachen.de/cgi-bin/luding/GameName.py?lang=DE&frames=0&gamename=' + jQuery('#Game_name').val(),'_blank');"
 			));
-		?>
-	</div>
-	<div class="row">
-		<?php echo $form->labelEx($model, 'luding_id'); ?>
-		<?php echo $form->textField($model, 'luding_id'); ?>
-		<?php echo $form->error($model, 'luding_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model, 'players'); ?>
-		<?php echo $form->textField($model, 'min_players', array('size' => 1, 'maxlength' => 10, 'disabled' => FALSE)); ?>
-		-
-		<?php echo $form->textField($model, 'max_players', array('size' => 1, 'maxlength' => 10, 'disabled' => FALSE)); ?>
-		<?php echo $form->error($model, 'min_players'); ?>
-
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model, 'min_age'); ?>
-		<?php echo $form->textField($model, 'min_age'); ?>
-		<?php echo $form->error($model, 'min_age'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model, 'duration'); ?>
-		<?php echo $form->textField($model, 'duration'); ?>
-		<?php echo $form->error($model, 'duration'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model, 'boxsize'); ?>
-		<?php
-		echo $form->textField($model, 'boxwidth', array(
-			'size' => 4,
-			'maxlength' => 4)
-		)
-		. ' x ' .
-		$form->textField($model, 'boxlength', array(
-			'size' => 4,
-			'maxlength' => 4
-				)
-		)
-		. ' x ' .
-		$form->textField($model, 'boxheight', array(
-			'size' => 4,
-			'maxlength' => 4
-				)
-		);
-		?>
-		<?php echo $form->error($model, 'boxwidth'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model, 'categories'); ?>
-		<?php
-		echo $form->listBox($model, 'categoryIds', CHtml::listData(Category::model()->findAll(), 'id', 'name'), array(
-			'multiple' => true,
-			'checkAll' => 'Check All',
-			'style' => 'width:200px',
-		));
+			echo $form->error($model, 'name'); 
 		?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'publisher_id'); ?>
-		<?php
-		echo $form->dropDownList($model, 'publisher_id', Publisher::model()->getPublisherOptions(), array(
-			'empty' => '-- Select publisher --',
-			'style' => 'width:200px',
-				)
-		);
+		<?php 
+			echo $form->labelEx($model, 'luding_id');
+			echo $form->textField($model, 'luding_id');
+			echo $form->error($model, 'luding_id'); 
 		?>
-		<?php echo $form->error($model, 'publisher_id'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'artists'); ?>
-		<?php $this->renderPartial('_artists', array('model' => $model)); ?>
+		<?php 
+			echo $form->labelEx($model, 'players');
+			echo $form->textField($model, 'min_players', array('size' => 1, 'maxlength' => 10, 'disabled' => FALSE));
+			echo " - ";
+			echo $form->textField($model, 'max_players', array('size' => 1, 'maxlength' => 10, 'disabled' => FALSE));
+			echo $form->error($model, 'min_players'); 
+		?>
+
 	</div>
 
 	<div class="row">
-		<?php echo CHtml::textField('new_artist', '', 
-				array(
+		<?php 
+			echo $form->labelEx($model, 'min_age');
+			echo $form->textField($model, 'min_age');
+			echo $form->error($model, 'min_age'); 
+		?>
+	</div>
+
+	<div class="row">
+		<?php 
+			echo $form->labelEx($model, 'duration');
+			echo $form->textField($model, 'duration');
+			echo $form->error($model, 'duration'); 
+		?>
+	</div>
+
+	<div class="row">
+		<?php 
+			echo $form->labelEx($model, 'boxsize');
+			$minifield = array('size' => 4, 'maxlength' => 4);
+			echo $form->textField($model, 'boxwidth', $minifield) . ' x ' . $form->textField($model, 'boxlength', $minifield) . ' x ' . $form->textField($model, 'boxheight', $minifield);
+			echo $form->error($model, 'boxwidth'); 
+		?>
+	</div>
+
+	<div class="row">
+		<?php 
+			echo $form->labelEx($model, 'categories');
+			echo $form->listBox($model, 'categoryIds', 
+				CHtml::listData(Category::model()->findAll(), 'id', 'name'), array(
+					'multiple' => true,
+					'checkAll' => 'Check All',
 					'style' => 'width:200px',
-					'placeholder' => 'New artist',
 				)
-			); ?> 
-		<?php
-		echo CHtml::ajaxSubmitButton(
-				' + ', array('game/reqUpdateArtist'), 
-				array(
-					'data' => 'js:jQuery(this).parents("form").serialize()+"&isAjaxRequest=1"',
-					'success' => 'function (data) {
-								$("#Game_artistIds").html(data);
-								$("#new_artist").val("");
-							}',
-				), 
-				array(
-					'id' => 'new_artist_ajax',
-					'name' => 'new_artist_ajax'
-				)
-		);
+			);
 		?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model, 'authors'); ?>
-		<?php $this->renderPartial('_authors', array('model' => $model)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo CHtml::textField('new_author', '', 
-				array(
-					'style' => 'width:200px',
-					'placeholder' => 'New author'
-				)); ?>
-		<?php
-		echo CHtml::ajaxSubmitButton(
-				' + ', array('game/reqUpdateAuthor'), array(
-			'data' => 'js:jQuery(this).parents("form").serialize()+"&isAjaxRequest=1"',
-			'success' => 'function (data) {
-						$("#Game_authorIds").html(data);
-						$("#new_author").val("");
-					}',
-				), array(
-			'id' => 'new_author_ajax',
-			'name' => 'new_author_ajax'
-				)
-		);
-		?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model, 'base_game_id'); ?>
-		<?php echo $form->dropDownList($model, 'base_game_id', $model->getPotentialBaseGameOptions(), array('empty' => '-- Select base game --')); ?>
-		<?php echo $form->error($model, 'base_game_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model, "Publisher"); ?>
-		<?php echo $form->hiddenField($model, 'publisher_id', array()); ?>
-		<?php $this->widget('zii.widgets.jui.CJuiAutoComplete',
+		<?php 
+			echo $form->label($model, "Publisher");
+			echo $form->hiddenField($model, 'publisher_id', array());
+			$this->widget('zii.widgets.jui.CJuiAutoComplete',
 				array(
 					'model' => $model,
 					'attribute' => 'publishers_name',
@@ -183,12 +104,80 @@
 						'showAnim' => 'fold',
 						'select' => "js:function(publisher, ui) {
 							$('#Game_publisher_id').val(ui.item.id);
-							}"
+							$('#Game_publishers_name').val(ui.item.name);
+						}"
 						),
 					'cssFile' => false,
-				)); ?>
+				)
+			); 
+			echo $form->error($model, 'publisher_id'); 
+		?>
 	</div>
 	
+	<div class="row">
+		<div class="span-8">
+			<?php 
+				echo $form->labelEx($model, 'artists');
+				$this->renderPartial('_artists', array('model' => $model));
+				echo CHtml::textField('new_artist', '', 
+					array(
+						'style' => 'width:200px',
+						'placeholder' => 'New artist',
+					)
+				);
+				echo CHtml::ajaxSubmitButton(
+					' + ', array('game/reqUpdateArtist'), 
+					array(
+						'data' => 'js:jQuery(this).parents("form").serialize()+"&isAjaxRequest=1"',
+						'success' => 'function (data) {
+									$("#Game_artistIds").html(data);
+									$("#new_artist").val("");
+								}',
+					), 
+					array(
+						'id' => 'new_artist_ajax',
+						'name' => 'new_artist_ajax'
+					)
+				);
+			?>
+		</div>
+		
+		<div class="span-8 last">
+			<?php 
+				echo $form->labelEx($model, 'authors');
+				$this->renderPartial('_authors', array('model' => $model));
+				echo CHtml::textField('new_author', '', 
+					array(
+						'style' => 'width:200px',
+						'placeholder' => 'New author'
+					)
+				);
+			
+				echo CHtml::ajaxSubmitButton(
+					' + ', array('game/reqUpdateAuthor'), array(
+						'data' => 'js:jQuery(this).parents("form").serialize()+"&isAjaxRequest=1"',
+						'success' => 'function (data) {
+									$("#Game_authorIds").html(data);
+									$("#new_author").val("");
+								}',
+					), array(
+						'id' => 'new_author_ajax',
+						'name' => 'new_author_ajax'
+					)
+				);
+			?>
+		</div>
+		<hr />
+	</div>
+
+	<div class="row">
+		<?php 
+			echo $form->labelEx($model, 'base_game_id');
+			echo $form->dropDownList($model, 'base_game_id', $model->getPotentialBaseGameOptions(), array('empty' => '-- Select base game --'));
+			echo $form->error($model, 'base_game_id'); 
+		?>
+	</div>
+
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
