e1539d6bb57d893b93a18c08d101f647dea542a8
Udo Woitek <superudo@gmail.com>
2013-04-18 22:57:18 +0200

    overlay versuch

--- protected/views/game/test.php
+++ protected/views/game/test.php
@@ -9,55 +9,24 @@
 	'id'=>'game-form',
 	'enableAjaxValidation'=>false,
 )); ?>
-<!--
-	<div class="row">
-		<?php echo $form->labelEx($model, 'players'); ?>
-		<?php $this->widget('application.extensions.selectToUISlider.ESelectToUISlider',
-			array(
-				'model' => $model,
-				'properties' => array('min_players', 'max_players'),
-				'values' => $model->getPlayerCountValues(),
-				'htmlOptions' => array(
-					'hidden' => true
-				),
-			)
-		); ?>
-	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model, 'min_age'); ?>
-		<?php $this->widget('application.extensions.selectToUISlider.ESelectToUISlider',
-			array(
-				'model' => $model,
-				'properties' => array('min_age'),
-				'values' => $model->getAgeStepValues(),
-				'htmlOptions' => array('hidden' => true),
-			)
-		); ?>
-	</div>
--->
 <div class="row">
 	<?php
 		echo $form->textField($model, 'min_players');
 		echo $form->textField($model, 'max_players');
-		$this->widget('zii.widgets.jui.CJuiSliderInput', array(
-			'model' => $model,
-			'attribute' => 'min_players',
-			'maxAttribute' => 'max_players',
-			'event' => 'change',
-			// additional javascript options for the slider plugin
-			'options' => array(
-				'range' => true,
-				'min' => 1,
-				'max' => 10,
-				'step' => 1,
-				'slide' => 'js:function(ev,ui) {
-					$("#Game_min_players").val(ui.values[0]);
-					$("#Game_max_players").val(ui.values[1]);
-				}',
-			),
+	?>
+</div>
+
+<div class="row">
+	<?php
+		echo CHtml::button('Show', array(
+			'onclick' => 'jQuery("#overlay1").overlay();'
 		));
 	?>
+</div>
+
+<div class="simple_overlay" id="overlay1">
+	<p>This is a simple overlay.</p>
 </div>
 
 <div class="row buttons">
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -14,6 +14,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/gamesdb.css" />
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
